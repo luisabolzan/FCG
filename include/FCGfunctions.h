9420a6c00463dafa98bb63739dc1a8a4af4550f4
Rafael <rafabandeira4@gmail.com>
2025-11-05 20:58:32 -0300

    Exclusão de FCGincludes.h

--- include/FCGfunctions.h
+++ include/FCGfunctions.h
@@ -2,29 +2,48 @@
 #ifndef FCGFUNCTIONS_H
 #define FCGFUNCTIONS_H
 
-#include "FCGincludes.h"
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <tiny_obj_loader.h>
+#include <stb_image.h>
+#include "matrices.h"
+
 #include "FCGstructures.h"
 #include "globals.h"
 #include "utils.h"
 #include "matrices.h"
 
-// Declaração de funções utilizadas para pilha de matrizes de modelagem.
 void PushMatrix(glm::mat4 M);
 void PopMatrix(glm::mat4& M);
 
-void BuildTrianglesAndAddToVirtualScene(ObjModel*);         // Constrói representação de um ObjModel como malha de triângulos para renderização
-void ComputeNormals(ObjModel* model);                       // Computa normais de um ObjModel, caso não existam.
-void LoadShadersFromFiles();                                // Carrega os shaders de vértice e fragmento, criando um programa de GPU
-void LoadTextureImage(const char* filename);                // Função que carrega imagens de textura
-void DrawVirtualObject(const char* object_name);            // Desenha um objeto armazenado em g_VirtualScene
-GLuint LoadShader_Vertex(const char* filename);             // Carrega um vertex shader
-GLuint LoadShader_Fragment(const char* filename);           // Carrega um fragment shader
-void LoadShader(const char* filename, GLuint shader_id);    // Função utilizada pelas duas acima
-GLuint CreateGpuProgram(GLuint vertex_shader_id, GLuint fragment_shader_id); // Cria um programa de GPU
-void PrintObjModelInfo(ObjModel*);                          // Função para debugging
+void BuildTrianglesAndAddToVirtualScene(ObjModel*);
+void ComputeNormals(ObjModel* model);
+void LoadShadersFromFiles();
+void LoadTextureImage(const char* filename);
+void DrawVirtualObject(const char* object_name);
+GLuint LoadShader_Vertex(const char* filename);
+GLuint LoadShader_Fragment(const char* filename);
+void LoadShader(const char* filename, GLuint shader_id);
+GLuint CreateGpuProgram(GLuint vertex_shader_id, GLuint fragment_shader_id);
+void PrintObjModelInfo(ObjModel*);
 
-// Declaração de funções auxiliares para renderizar texto dentro da janela
-// OpenGL. Estas funções estão definidas no arquivo "textrendering.cpp".
+
 void TextRendering_Init();
 float TextRendering_LineHeight(GLFWwindow* window);
 float TextRendering_CharWidth(GLFWwindow* window);
@@ -34,16 +53,11 @@ void TextRendering_PrintVector(GLFWwindow* window, glm::vec4 v, float x, float y
 void TextRendering_PrintMatrixVectorProduct(GLFWwindow* window, glm::mat4 M, glm::vec4 v, float x, float y, float scale = 1.0f);
 void TextRendering_PrintMatrixVectorProductMoreDigits(GLFWwindow* window, glm::mat4 M, glm::vec4 v, float x, float y, float scale = 1.0f);
 void TextRendering_PrintMatrixVectorProductDivW(GLFWwindow* window, glm::mat4 M, glm::vec4 v, float x, float y, float scale = 1.0f);
-
-// Funções abaixo renderizam como texto na janela OpenGL algumas matrizes e
-// outras informações do programa. Definidas após main().
 void TextRendering_ShowModelViewProjection(GLFWwindow* window, glm::mat4 projection, glm::mat4 view, glm::mat4 model, glm::vec4 p_model);
 void TextRendering_ShowEulerAngles(GLFWwindow* window);
 void TextRendering_ShowProjection(GLFWwindow* window);
 void TextRendering_ShowFramesPerSecond(GLFWwindow* window);
 
-// Funções callback para comunicação com o sistema operacional e interação do
-// usuário. Veja mais comentários nas definições das mesmas, abaixo.
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
 void ErrorCallback(int error, const char* description);
 
